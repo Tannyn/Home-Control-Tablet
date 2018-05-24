3c3ecc54db2d994171bc9c7349fda8dbeb8b2196
Tannyn <yieldthefox@gmail.com>
2018-05-23 23:17:36 -0500

    More developing

--- Home-Control-Tablet/Home-Control-Tablet.ino
+++ Home-Control-Tablet/Home-Control-Tablet.ino
@@ -1,4 +1,19 @@
+#include <AdafruitIO.h>
+#include <AdafruitIO_Dashboard.h>
+#include <AdafruitIO_Data.h>
+#include <AdafruitIO_Definitions.h>
+
+#include <AdafruitIO_Feed.h>
+#include <AdafruitIO_Group.h>
+#include <AdafruitIO_MQTT.h>
+#include <AdafruitIO_WiFi.h>
+
+#include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Key.h>
 #include <Keypad.h>
+#include "config.h"
 
 const byte ROWS = 4;
 const byte COLS = 7;
@@ -12,12 +27,19 @@ char keys[ROWS][COLS] = {
 };
 
 //Connect (4) ROW pins here
-byte rowPins[ROWS] = {/* Fill this in when hardware mod is planned */};
+byte rowPins[ROWS] = {4,5,2,16};
 //Connect (7) COL pins here
-byte colPins[COLS] = {/* Fill this in when hardware mod is planned */};
+byte colPins[COLS] = {0,15,13,12,14,1,3};
 
 // @section keypad
 Keypad kpd = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+
+/*for(int x = 0; x<ROWS; x++){
+  pinMode(rowPins[x], INPUT);
+}
+for(int x = 0; x<COLS; x++){
+  pinMode(colPins[x], INPUT);
+} */
 
 /* This is for if I decide to keep the RGB led functionality.. Uncomment section in setup too!
 
@@ -25,16 +47,30 @@ int rgbLed[3] = {0, 0, 0};
 
 */
 
+//setup 'digital' feed
+AdafruitIO_Feed *digital = io.feed("digital");
+
 void setup() {
+  
   // put your setup code here, to run once:
   /* for(int i=0;x<3;x++){
     pinMode(rgbLed[i], OUTPUT);
     digitalWrite(rgbLed[i], LOW);} */
-    Serial.begin(9600);
+
+  //Start serial connection and wait for it to initialize
+  Serial.begin(115200);
+  while(! Serial);
+  
+  //connect to io.adafruit.com
+  Serial.print("Connecting to Adafruit");
+  io.connect();
+  
 
 }
 
 void loop() {
+  io.run();
+  
   // put your main code here, to run repeatedly:
   char key = kpd.getKey();
   if(key) //check for a valid key
@@ -47,7 +83,11 @@ void loop() {
         break;
       default:
         Serial.println("Misread key");
+        //break;
     }
+    //digital->save(key);
   }
 
 }
+
+
